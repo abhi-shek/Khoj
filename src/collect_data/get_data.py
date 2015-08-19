c871c38a87b1e18a075d8547d6d2e3e333982805
not-null <himanshu.rawat@gmail.com>
2015-08-19 15:26:41 +1000

    minor changes

--- src/collect_data/get_data.py
+++ src/collect_data/get_data.py
@@ -28,28 +28,29 @@ def connect_twitter(access_details):
  # we need to get all the combinations of the hastags
  # for each combination we need to query
   hash_comb = list(Helper.Helper.get_hashtags_combinations(access_details['hashtags'].split(",")))
-  tweet_id,tweet_count = 1,1
+  tweet_id,tweet_count,search_request_count = 0,0,0
   file_name = "_".join(["tweets",Helper.Helper.get_current_datetime()])
   file_name = app_globals.APP_DATA_DIR+"/"+file_name+app_globals.APP_DATA_EXT
   for i,val in enumerate(hash_comb):
    if (0 == len(val)): continue
    else:
     hashtag_list = ",".join(map(str,val)) 
-    search = api.GetSearch(term=hashtag_list , lang='en', result_type='recent', count=10, max_id='')
+    search = api.GetSearch(term=hashtag_list , lang='en', result_type='mixed', count=100, max_id='')
+    search_request_count += 1
  # this one uses geolocation tweets (india only)
  # 
  #search = api.GetSearch(term=access_details['hashtags'], geocode='20.593684020,78.962880078,1km',lang='en', result_type='recent', count=10, max_id='')
     
     with open(file_name,"a") as ofile:
      for t in search:
-      line = "|".join([str(tweet_id),t.text.encode('utf-8'),"\n"])
+      line = "|".join([str(tweet_id+1),t.text.encode('utf-8'),"\n"])
       ofile.write(line)
       tweet_id,tweet_count = tweet_id + 1,tweet_count + 1
    logger.debug("Tweets count containing hashtag -> [%s][%d]", hashtag_list,tweet_count)
    tweet_count = 0
   # (CPU + Operation) time taken in seconds (getting + writing data to file)
 
-  logger.debug("(seconds) CPU time [%s], Operation time [%s]", (time.clock() - cpu_start_time), (time.time() - operation_start_time))
+  logger.debug("Total search request [%s], Tweet count [%s] in (seconds) CPU time [%s], Operation time [%s]", search_request_count,tweet_id,(time.clock() - cpu_start_time), (time.time() - operation_start_time))
 
  except Exception as te:
   logger.error(te, exc_info=True)
